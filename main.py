2cd9f9d426ba26c66835c0fd4baf7919366f2016
AlexKulam <alexkulam45@gmail.com>
2025-07-18 20:35:01 +0300

    добавлен фронтенд

--- main.py
+++ main.py
@@ -1,52 +1,36 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 books = [
-    {
-        "id": 1,
-        "title": "Ассинхроность в Python",
-        "author": "Стен Ли"
-    },
-    {
-        "id": 2,
-        "title": "Backend на Python",
-        "author": "Бен Робин"
-    },
+    {"id": 1, "title": "Асинхронность в Python", "author": "Стен Ли"},
+    {"id": 2, "title": "Backend на Python", "author": "Бен Робин"},
 ]
 
-@app.get(
-        "/books",
-        tags=["Книги"],
-        summary="Получить все книги"
-        )
+@app.get("/books")
 def read_books():
     return books
 
-
-@app.get(
-        "/books/{book_id}",
-        tags=["Книги"],
-        summary="Получить конретную книжку"
-        )
+@app.get("/books/{book_id}")
 def get_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
             return book
-
     raise HTTPException(status_code=404, detail="Книга не найдена")
 
 class NewBook(BaseModel):
     title: str
     author: str
 
-@app.post("/books", tags=["Книги"])
+@app.post("/books")
 def create_book(new_book: NewBook):
     books.append({
         "id": len(books) + 1,
         "title": new_book.title,
         "author": new_book.author
-
     })
     return {"success": True, "message": "Книга успешно добавлена"}
